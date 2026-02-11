f666d1f8e7b9205c22ff9a56f0220d0b0014e90b
Châteauneufsandrine <chateauneufsandrine188@gmail.com>
2026-02-11 14:47:28 +0100

    add exercice2

--- exercice2/main.js
+++ exercice2/main.js
@@ -1,4 +1,13 @@
-let nombre1 = 5
-let nombre2 = 10
+let nombre1 = 5;
+let nombre2 = 10;
 
-(nombre1 + nombre2);
+
+function additionner(nombre1, nombre2) {
+    return nombre1 + nombre2;
+}
+
+let resultat = additionner (nombre1, nombre2);
+  
+    console.log(resultat);
+
+
